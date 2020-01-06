1409ed39a8ffbcae800092f8aea38ca02367e84d
HP <949467994@qq.com>
2020-01-06 17:27:34 +0800

    test

--- metapath2vec/vec2csv.py
+++ metapath2vec/vec2csv.py
@@ -1,20 +1,21 @@
 import numpy as np
 import json
-
+import csv
+#生成csv文件用于聚类
+# 路径要改！！！！！！！！！！！！！！！
 dir = ".\\data\\ucu\\recommend"
 dirpath = ".\\data\\ucu\\vector"
 
-index2nodeid = json.load(open(dir+"\\log\\index2nodeid.json"))
+index2nodeid = json.load(open(dir +"\\log\\index2nodeid.json"))
 index2nodeid = {int(k): v for k, v in index2nodeid.items()}
 nodeid2index = {v: int(k) for k, v in index2nodeid.items()}
-node_embeddings = np.load(dir+"\\log\\node_embeddings.npz")['arr_0']
+node_embeddings = np.load(dir +"\\log\\node_embeddings.npz")['arr_0']
 # 100维的向量
-
 # 存放poi和user的id
 poilist = list()
 userlist = list()
+poi_cate = dict()
 
-#读取用户和poi列表
 def readnodetype():
     f = open(dirpath+'\\node_type_mapings.txt', 'r')
     line = f.readline()
@@ -28,65 +29,57 @@ def readnodetype():
         line = f.readline()
     f.close()
 
-id_cate = dict()
-poi_cate = dict()
 def poi_category():
-    #读id_cate
-    with open(".\\data\\100user\\id_category.txt", 'r', encoding='ISO-8859-1') as cdictfile:
-        for line in cdictfile:
-            toks = line.strip().split("\t")
-            if len(toks) == 2:
-                id_cate[toks[0]] = toks[1]
-    #构造poi-cate
-    with open(".\\data\\100user\\train.txt", 'r', encoding='ISO-8859-1') as pafile:
-        for line in pafile:
-            toks = line.strip().split("\t")
-            if len(toks) == 4:
-                p, c = toks[2], toks[3]
-                if p not in poi_cate:
-                    poi_cate[p] = id_cate[c]
-
+    f = open(dirpath+"\\poi_cate_index.txt", 'r')
+    line = f.readline()
+    while line:
+        list = line.strip().split("\t")
+        if (len(list) == 2):
+            if (list[0] not in poi_cate):
+                poi_cate[list[0]] = list[1]
+        line = f.readline()
+    f.close()
 def poi2csv():
     readnodetype()  # 构造poilist
-    #print(poilist)
     poi_category()  # 构造poi_cate字典
-    #print(poi_cate)
+    with open(dir+"\\vector.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        # 先写入columns_name # 写入多行用writerows
+        row = []
+        row.append('index')
+        for i in range(0, 100):
+            row.append('n' + str(i))
+        row.append('category')
+        writer.writerow(row)
 
-    fvec = open(dir+'\\vec.txt', 'w')
-    findex = open(dir+'\\index.txt', 'w')
+        for poi in poilist:
+            cate = poi_cate[poi]
+            vec = np.array(node_embeddings[nodeid2index[poi]])
+            temp = []
+            temp.append(poi)
+            for i in range(0, len(vec)):
+                temp.append(vec[i])
+            temp.append(cate)
+            writer.writerow(temp)
 
-    findex.write("poi"+"\t"+"category"+'\n')
-    for poi in poilist:
-        cate = poi_cate[poi]
-        vec = np.array(node_embeddings[nodeid2index[poi]])
-
-        findex.write(poi+"\t"+cate+'\n')
-
-        temp = str(vec[0])
-        for i in range(1,len(vec)):
-            temp = temp + '\t' + str(vec[i])
-        temp = temp + '\n'
-        fvec.write(temp)
-
-#不写属性标题，用于向量可视化
 def user2csv():
     readnodetype()  # 构造userlist
 
-    fvec = open(dir+'\\vec.txt', 'w')
-    findex = open(dir+'\\index.txt', 'w')
-    findex.write("user"+'\n')
+    with open(dir+"\\vector.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        # 先写入columns_name # 写入多行用writerows
+        row = []
+        row.append('index')
+        for i in range(0, 100):
+            row.append('n' + str(i))
+        writer.writerow(row)
 
-    for u in userlist:
-        vec = np.array(node_embeddings[nodeid2index[u]])
-        findex.write(u+'\n')
+        for u in userlist:
+            vec = np.array(node_embeddings[nodeid2index[u]])
+            temp = []
+            temp.append(u)
+            for i in range(0, len(vec)):
+                temp.append(vec[i])
+            writer.writerow(temp)
 
-        fvec.write(u + '\t')
-        temp = str(vec[0])
-        for i in range(1,len(vec)):
-            temp = temp + '\t' + str(vec[i])
-        temp = temp + '\n'
-        fvec.write(temp)
-
-if __name__ == "__main__":
-
-    user2csv()  # id向量类别信息写入csv文件 可以用weka进行聚类分类
+user2csv()
